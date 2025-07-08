c4d7cd43d5f55fd0bf27a2bff8cdfa8bbd6f6931
86 <ansmayo86@gmail.com>
2025-07-08 07:07:07 +0500

    Initial commit

---
+++ ans/ans/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>/?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed =analyzed + char
+
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif(fullcaps=="on"):
+        analyzed=""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+            params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif(newlineremover=="on"):
+         analyzed=""
+         for char in djtext:
+            if char !="\n":
+             analyzed = analyzed + char
+            params = {'purpose': 'Removed New Lines', 'analyzed_text': analyzed}
+            return render(request, 'analyze.html', params)
+         
+    elif(extraspaceremover=="on"):
+         analyzed=""
+         for index, char in enumerate(djtext):
+            if djtext[index] == " " and djtext[index+1]==" ":
+             pass
+            else:
+             analyzed = analyzed + char
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+            return render(request, 'analyze.html', params)
+
+    
+    else:
+        return HttpResponse("⚠️ Please check atleast one box ⚠️")
